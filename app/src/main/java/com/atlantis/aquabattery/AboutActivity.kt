125fca5de096877d540ff5de09f46b15feeb9e24
MODEDGES <ofwargane30@gmail.com>
2025-12-28 19:43:54 +0000

    v1.1: Add About menu with version information

--- app/src/main/java/com/atlantis/aquabattery/AboutActivity.kt
+++ app/src/main/java/com/atlantis/aquabattery/AboutActivity.kt
@@ -1,7 +1,9 @@
 package com.atlantis.aquabattery
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class AboutActivity : AppCompatActivity() {
 
@@ -9,8 +11,17 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
+        // Toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "About"
+
+        // Version text
+        val tvVersion = findViewById<TextView>(R.id.tvVersion)
+        tvVersion.text = "Version " + packageManager
+        .getPackageInfo(packageName, 0)
+        .versionName
     }
 
     override fun onSupportNavigateUp(): Boolean {
